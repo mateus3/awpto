d4e8b0a605a40e2d36b75612bcecdf4294b71cc4
mateus3 <mateus3@onet.eu>
2019-12-30 23:13:46 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Testowa aplikacja na APWTO <span role="img" aria-label="">👏</span>
+        </p>
+        <p>Huraaa DZIAŁA</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
