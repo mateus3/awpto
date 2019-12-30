ed750d1030a8720103d81bc2d77402393afafd7c
mateus3 <mateus3@onet.eu>
2019-12-30 23:03:12 +0000

    Updates

---
+++ precache-manifest.017e8e9d6c2331b77dd7601af59f5b57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86621b8b4a752dcf132bdbd4c76d840b",
+    "url": "/awpto/index.html"
+  },
+  {
+    "revision": "9280fe4e40ec0ef83bbe",
+    "url": "/awpto/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "fd8ef51410b535c818a7",
+    "url": "/awpto/static/js/2.eb808ce8.chunk.js"
+  },
+  {
+    "revision": "9280fe4e40ec0ef83bbe",
+    "url": "/awpto/static/js/main.f5671088.chunk.js"
+  },
+  {
+    "revision": "9ee848f63c7a9eaf5ff3",
+    "url": "/awpto/static/js/runtime-main.8c798421.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/awpto/static/media/logo.25bf045c.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/awpto/precache-manifest.017e8e9d6c2331b77dd7601af59f5b57.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/awpto/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
